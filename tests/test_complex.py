79caec32c4d059caf551c23fc233f759df092f5a
Elias Dalan <ceadyy@gmail.com>
2026-06-03 11:07:17 +0200

    test: added test setup and first test

---
+++ tests/test_complex.py
@@ -0,0 +1,28 @@
+import pytest
+from src.complex import Complex
+
+@pytest.fixture
+def z1():
+    return Complex(2, 3)
+
+@pytest.fixture
+def z2():
+    return Complex(1, 4)
+
+def test_addition(z1, z2):
+    result = z1 + z2
+
+    assert result.real == 3
+    assert result.imag == 7
+
+def test_subtraction(z1, z2):
+    result = z1 - z2
+
+    assert result.real == 1
+    assert result.imag == -1
+
+def test_multiplication(z1, z2):
+    result = z1 * z2
+
+    assert result.real == -10
+    assert result.imag == 11
